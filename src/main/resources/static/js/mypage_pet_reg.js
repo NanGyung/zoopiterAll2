a5edeb6f2518200895bfbb43838db47954da04bc
FoHam <spodos37@naver.com>
2023-04-06 17:37:26 +0900

    은아님 추가

--- src/main/resources/static/js/mypage_pet_reg.js
+++ src/main/resources/static/js/mypage_pet_reg.js
@@ -63,3 +63,38 @@ function DropFile(dropAreaId, fileListId) {
 }
 
 const dropFile = new DropFile('drop-file', 'files');
+
+/* 등록완료 */
+function modify() {
+  Swal.fire({
+    title: '등록하시겠습니까?',
+    text: '작성하신 정보가 등록됩니다',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#333',
+    cancelButtonColor: '#ffd88f',
+    confirmButtonText: '등록하기',
+    cancelButtonText: '취소하기',
+  }).then(result => {
+    if (result.isConfirmed) {
+      location.replace('./mypage_main.html');
+    }
+  });
+}
+
+function petModify() {
+  Swal.fire({
+    title: '수정페이지로 이동하시겠습니까?',
+    text: '등록한 반려동물정보를 수정하러 이동합니다',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#333',
+    cancelButtonColor: '#ffd88f',
+    confirmButtonText: '수정하기',
+    cancelButtonText: '취소하기',
+  }).then(result => {
+    if (result.isConfirmed) {
+      location.replace('./mypage_pet_modify.html');
+    }
+  });
+}
