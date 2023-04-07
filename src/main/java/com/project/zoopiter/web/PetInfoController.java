9a504c5b1785727d77d15443f1254c7ca953a6e6
FoHam <spodos37@naver.com>
2023-04-07 16:38:18 +0900

    pet 변경일지도

--- src/main/java/com/project/zoopiter/web/PetInfoController.java
+++ src/main/java/com/project/zoopiter/web/PetInfoController.java
@@ -1,40 +1,36 @@
 package com.project.zoopiter.web;
 
 import com.project.zoopiter.domain.member.svc.PetInfoSVC;
-import com.project.zoopiter.web.login.SaveInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/pet")
+@RequestMapping("/mypage/pet")
 @RequiredArgsConstructor
 public class PetInfoController {
   private final PetInfoSVC petInfoSVC;
 // 등록 pet_reg
-  // 등록양식
+  // 등록화면
   @GetMapping("/pet_reg")
-  public String saveInfo(Model model){
+  public String saveInfo(){
 //    PetInfo petInfo = new PetInfo();
 //    String save = petInfoSVC.saveInfo(petInfo);
-    model.addAttribute("saveInfo", new SaveInfo());
     return "/mypage/mypage_pet_reg";
   }
   // 등록처리
 //  PetInfo saveInfo(PetInfo petInfo);
   @PostMapping("/pet_reg")
-  public String save(@ModelAttribute SaveInfo saveInfo){
-    return "redirect:/mypage/pet/{id}/mypage_pet_reg";
+  public String save(){
+    return "redirect:/mypage/pet/{id}/detail";
   }
 
   // 조회
   @GetMapping("/{id}/detail")
   public String findInfo(){
-    return "../static/html/pet_modify";
+    return "/mypage/pet_modify";
   }
 
 // 수정 pet_modify > 메인으로 이동(보호자정보페이지)
@@ -42,20 +38,20 @@ public class PetInfoController {
   // 수정양식
   @GetMapping("/{id}/edit")
   public String updateInfo(){
-    return "../static/html/pet_modify";
+    return "/mypage/pet_modify";
   }
 
   // 수정
   @PostMapping("/{id}/edit")
   public String update(){
-    return "redirect:/mypage/pet_modify";
+    return "redirect:/mypage/pet/{id}/edit";
   }
 
   // 삭제 > 메인으로 이동(보호자정보페이지)
 //  int deleteInfo(Long PetNum);
   @GetMapping("{id}/del")
   public String deleteInfo(){
-    return "redirect:/mypage_main";
+    return "redirect:/mypage/pet";
   }
 
   // 목록 > ?
--- src/main/java/com/project/zoopiter/web/login/SaveInfo.java
+++
@@ -1,7 +0,0 @@
-package com.project.zoopiter.web.login;
-
-import lombok.Data;
-
-@Data
-public class SaveInfo {
-}
