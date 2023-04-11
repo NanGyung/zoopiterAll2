b88f23229e8fbe1581911ae08d45efe0394c5123
“NanGyung” <rhrnakaktdidrod0926@gmail.com>
2023-04-11 17:52:43 +0900

    회원가입&로그인 화면 레이아웃 수정

---
+++ src/main/java/com/project/zoopiter/web/RestMemverController.java
@@ -0,0 +1,70 @@
+package com.project.zoopiter.web;
+
+import com.project.zoopiter.domain.entity.Member;
+import com.project.zoopiter.domain.member.svc.MemberSVC;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController // @Controller + @ResponseBody
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class RestMemverController {
+
+    private final MemberSVC memberSVC;
+
+    //회원닉네임 체크
+    @ResponseBody
+    @GetMapping("/nickname")
+    public RestResponse<Object> isExistNick(@RequestParam("nickname") String nickname){
+        log.info("nickname={}",nickname);
+        RestResponse<Object> res = null;
+
+        //아이디 검증
+        boolean exist = memberSVC.isExistNick(nickname);
+        res = RestResponse.createRestResponse("00","성공", exist);
+
+        return res;
+    }
+
+    //회원아이디 체크
+    @ResponseBody
+    @GetMapping("/id")
+    public RestResponse<Object> isExistId(@RequestParam("id") String id){
+        log.info("id={}",id);
+        RestResponse<Object> res = null;
+
+        //아이디 검증
+        boolean exist = memberSVC.isExistId(id);
+        res = RestResponse.createRestResponse("00","성공", exist);
+
+        return res;
+    }
+
+    //회원이메일 체크
+    @ResponseBody
+    @GetMapping("/email")
+    public RestResponse<Object> isExistEmail(@RequestParam("email") String email){
+        log.info("email={}",email);
+        RestResponse<Object> res = null;
+
+        //아이디 검증
+        boolean exist = memberSVC.isExistEmail(email);
+        res = RestResponse.createRestResponse("00","성공", exist);
+
+        return res;
+    }
+
+    @ResponseBody
+    @PostMapping("/signup1")
+    public RestResponse<Object> signupSave(@RequestBody Member member){
+        RestResponse<Object> res = null;
+
+        //회원기입
+        Member member2 = memberSVC.save(member);
+        res = RestResponse.createRestResponse("00","성공",member2);
+
+        return res;
+    }
+}
---
+++ src/main/resources/static/js/member_signup2.js
@@ -0,0 +1,313 @@
+import { ajax } from '/js/ajax.js';
+
+const $id = document.getElementById('id');
+const $pw = document.getElementById('pw');
+const $pwCheck = document.getElementById('pwCheck');
+const $email = document.getElementById('email');
+const $emailCheck = document.getElementById('emailCheck');
+
+const $errNickname = document.querySelector('.err.nickname');
+const $errId = document.querySelector('.err.id');
+const $errPw = document.querySelector('.err.pw');
+const $errPwCheck = document.querySelector('.err.pwCheck');
+const $errEmail = document.querySelector('.err.email');
+const $errEmailCheck = document.querySelector('.err.emailCheck');
+
+//아이디 중복체크
+const chkId = res => {
+  if (res.header.rtcd == '00') {
+    if (res.data) {
+      $errId.style = 'color : red';
+      $errId.textContent = '사용중인 아이디 입니다.';
+    } else {
+      $errId.style = 'color : green';
+      $errId.textContent = '사용가능한 아이디 입니다.';
+    }
+  } else {
+    $errId.textContent = `${res.header.rtmsg}`;
+  }
+};
+
+const chkId_h = () => {
+  const url = `/api/members/id?id=${$id.value}`;
+  ajax
+    .get(url)
+    .then(res => res.json())
+    .then(chkId) //res=>chkEmail(res)
+    .catch(console.error); //err=>console.error(err)
+};
+
+//아이디
+$id.addEventListener('keydown', e => {
+  const input = $id.value;
+  const lenOfInput = input.length;
+
+  if (e.key === ' ') {
+    e.preventDefault();
+  }
+
+  if (e.key != 'Enter') {
+    return;
+  }
+  if (e.key == 'Enter') {
+    if (/[^A-Za-z0-9]/.test(input)) {
+      $errId.classList.remove('hidden');
+      $errId.style = 'color : red';
+      $errId.textContent = '* 영문 숫자만 입력 가능합니다.';
+      $id.focus();
+      return;
+    }
+    if (lenOfInput == 0) {
+      $errId.classList.remove('hidden');
+      $errId.style = 'color : red';
+      $errId.textContent = '* 아이디를 입력해 주세요.';
+      $id.focus();
+      $id.value = '';
+    } else {
+      //      $errId.classList.add('hidden');
+      // $id.value = input;
+      chkId_h();
+      $pw.focus();
+    }
+    return;
+  }
+});
+
+$id.addEventListener('blur', e => {
+  const input = $id.value.trim();
+  const lenOfInput = input.length;
+
+  if (/[^A-Za-z0-9]/.test(input)) {
+    $errId.classList.remove('hidden');
+    $errId.style = 'color : red';
+    $errId.textContent = '* 영문 숫자만 입력 가능합니다.';
+    return;
+  }
+  if (lenOfInput == 0) {
+    $errId.classList.remove('hidden');
+    $errId.style = 'color : red';
+    $errId.textContent = '* 아이디을 입력해 주세요.';
+    $id.value = '';
+  } else {
+    //    $errId.classList.add('hidden');
+    chkId_h();
+    // $id.value = input;
+  }
+  return;
+});
+
+//비밀번호
+$pw.addEventListener('keydown', e => {
+  const input = $pw.value;
+  const lenOfInput = input.length;
+
+  if (e.key === ' ') {
+    e.preventDefault();
+  }
+
+  if (e.key != 'Enter') {
+    return;
+  }
+  if (e.key == 'Enter') {
+    if (/[^A-Za-z0-9]/.test(input)) {
+      $errPw.classList.remove('hidden');
+      $errPw.textContent = '* 영문 대소문자 숫자만 입력 가능합니다.';
+      $pw.focus();
+      return;
+    }
+    if (lenOfInput == 0) {
+      $errPw.classList.remove('hidden');
+      $errPw.textContent = '* 비밀번호를 입력해 주세요.';
+      $pw.focus();
+    } else if (8 > lenOfInput || lenOfInput > 20) {
+      $errPw.classList.remove('hidden');
+      $errPw.textContent = '* 비밀번호는 8~20자 입력 가능합니다.';
+      $pw.focus();
+    } else {
+      $errPw.classList.add('hidden');
+      $pwCheck.focus();
+    }
+    return;
+  }
+});
+
+$pw.addEventListener('blur', e => {
+  const input = $pw.value;
+  const lenOfInput = input.length;
+
+  if (/[^A-Za-z0-9]/.test(input)) {
+    $errPw.classList.remove('hidden');
+    $errPw.textContent = '* 영문 대소문자 숫자만 입력 가능합니다.';
+    return;
+  }
+  if (lenOfInput == 0) {
+    $errPw.classList.remove('hidden');
+    $errPw.textContent = '* 비밀번호를 입력해 주세요.';
+  } else if (8 > lenOfInput || lenOfInput > 20) {
+    $errPw.classList.remove('hidden');
+    $errPw.textContent = '* 비밀번호는 8~20자 입력 가능합니다.';
+  } else {
+    $errPw.classList.add('hidden');
+  }
+  return;
+});
+
+$pwCheck.addEventListener('focus', e => {
+  if ($pw.value == null) {
+    $pw.focus();
+  } else if (!$errPw.classList.contains('hidden')) {
+    $pw.focus();
+  }
+});
+
+//비밀번호 확인
+$pwCheck.addEventListener('input', e => {
+  const input = $pwCheck.value;
+  const inputChk = $pw.value;
+  const lenOfInput = input.length;
+
+  if (lenOfInput != 0) {
+    if (input == inputChk) {
+      $errPw.classList.add('hidden');
+      $errPwCheck.style = 'color : green';
+      $errPwCheck.textContent = '비밀번호가 일치합니다';
+    } else {
+      $errPwCheck.classList.remove('hidden');
+      $errPwCheck.style = 'color : red';
+      $errPwCheck.textContent = '비밀번호가 일치하지 않습니다.';
+    }
+  }
+  return;
+});
+
+//비밀번호 확인
+$pwCheck.addEventListener('keydown', e => {
+  const input = $pwCheck.value;
+  const inputChk = $pw.value;
+  const lenOfInput = input.length;
+
+  if (e.key === ' ') {
+    e.preventDefault();
+  }
+
+  if (e.key != 'Enter') {
+    return;
+  }
+
+  if (e.key == 'Enter') {
+    if (lenOfInput != 0 && input == inputChk) {
+      $email.focus();
+    }
+  }
+  return;
+});
+
+//이메일 중복체크
+const chkEmail = res => {
+  if (res.header.rtcd == '00') {
+    if (res.data) {
+      $errEmail.style = 'color : red';
+      $errEmail.textContent = '사용중인 이메일 입니다.';
+    } else {
+      $errEmail.style = 'color : green';
+      $errEmail.textContent = '사용가능한 이메일 입니다.';
+    }
+  } else {
+    $errEmail.textContent = `${res.header.rtmsg}`;
+  }
+};
+
+const chkEmail_h = () => {
+  const url = `/api/members/email?email=${email.value}`;
+  ajax
+    .get(url)
+    .then(res => res.json())
+    .then(chkEmail) //res=>chkEmail(res)
+    .catch(console.error); //err=>console.error(err)
+};
+
+//이메일
+$email.addEventListener('keydown', e => {
+  const input = $email.value;
+  const lenOfInput = input.length;
+  const emailRegex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@(?:\w+\.)+\w+$/;
+
+  if (e.key === ' ') {
+    e.preventDefault();
+  }
+
+  if (e.key != 'Enter') {
+    return;
+  }
+  if (e.key == 'Enter') {
+    if (lenOfInput == 0) {
+      $errEmail.classList.remove('hidden');
+      $errEmail.style = 'color : red';
+      $errEmail.textContent = '* 이메일를 입력해 주세요.';
+      $email.focus();
+      $email.value = '';
+    } else if (!emailRegex.test(input)) {
+      $errEmail.classList.remove('hidden');
+      $errEmail.style = 'color : red';
+      $errEmail.textContent = '* 이메일 양식에 맞게 입력해 주세요.';
+      $email.focus();
+    } else {
+      //      $errEmail.classList.add('hidden');
+      //      $email.value = input;
+      chkEmail_h();
+      $emailCheck.focus();
+    }
+    return;
+  }
+});
+
+$email.addEventListener('blur', e => {
+  const input = $email.value;
+  const lenOfInput = input.length;
+  const emailRegex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@(?:\w+\.)+\w+$/;
+
+  if (lenOfInput == 0) {
+    $errEmail.classList.remove('hidden');
+    $errEmail.style = 'color : red';
+    $errEmail.textContent = '* 이메일를 입력해 주세요.';
+    $email.value = '';
+  } else if (!emailRegex.test(input)) {
+    $errEmail.classList.remove('hidden');
+    $errEmail.style = 'color : red';
+    $errEmail.textContent = '* 이메일 양식에 맞게 입력해 주세요.';
+  } else {
+    //    $errEmail.classList.add('hidden');
+    //    $email.value = input;
+    chkEmail_h();
+  }
+  return;
+});
+
+//member 내보내기
+//const member_h = () => {
+//  const url = `/api/members/signup2`;
+//  const payLoad = {
+//    userId: $id.value,
+//    userPw: $pw.value,
+//    userEmail: $email.value,
+//  };
+//  ajax
+//    .post(url, payLoad)
+//    .then(res => res.json())
+//    .catch(console.error); //err=>console.error(err)
+//};
+
+const $loginBtn = document.getElementById('loginBtn');
+const $loginPopup = document.getElementById('loginPopup');
+$loginBtn.addEventListener('click', e => {
+  if (
+    $errId.style.color === 'green' &&
+    $errEmail.style.color === 'green' &&
+    $errPwCheck.style.color === 'green'
+  ) {
+    //    member_h();
+    $loginPopup.showModal();
+  } else {
+    alert('회원가입 양식에 맞게 입력해 주세요');
+  }
+});
